6480141ec0ec6a4bf08490c49f89b65713451960
Jmendez <jorge.mendez.ortega@gmail.com>
2019-03-15 10:54:28 -0600

    ADD: Config file
    Se agrega el archivo de configuracion base
    
    Issue #5

--- src/react-component.js
+++ src/react-component.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Master } from "./react";
-import Scroll from "./infiniteScroll/Scroll";
+import Request from "./infiniteScroll/Request";
 
 document.onreadystatechange = () => {
     const { readyState } = document;
     if (readyState === "complete") {
-        // const CONFIG = {
-        //     url: "http://local.televisa.com:3000/data.json",
-        //     type: "ArticlePage",
-        // };
-        // const REQUEST = new Request(CONFIG);
-        // REQUEST.then(data => console.log(data));
-        Scroll.setActionScroll();
+        const CONFIG = {
+            url: "http://local.televisa.com:3000/data.json",
+            type: "ArticlePage",
+        };
+        const REQUEST = new Request(CONFIG);
+        REQUEST.then(data => console.log(data));
     }
 };
 
